e78cc0c3fcb6b87473614226783d66e592a186b8
nazir-ahmad-khan <nazirahmadfn@gmail.com>
2026-08-14 14:36:59 +0500

    Add root Dockerfile for Back4App deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,47 +1,62 @@
 // config mein application ki configuration-related files hoti hain.
 // db.js MongoDB connection handle karega.
 
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI)
+
+//         console.log("MongoDB connected successfully");
+//     }
+//     catch (error) {
+//         console.error("MongoDB connection failed:", error.message);
+//         process.exit(1);
+//     }
+// }
+
+// module.exports = connectDB;
+
+
+
+
+
+// const dns = require("node:dns");
+// const mongoose = require("mongoose");
+
+// dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+
+//         console.log("MongoDB connected successfully");
+//     }
+//     catch (error) {
+//         console.error("MongoDB connection failed:", error.message);
+//         process.exit(1);
+//     }
+// };
+
+// module.exports = connectDB;
+
+
+
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log("MongoDB connected successfully");
-    }
-    catch (error) {
+    } catch (error) {
         console.error("MongoDB connection failed:", error.message);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-
-//     console.log("MongoDB connected successfully");
-//   } catch (error) {
-//     console.error("MongoDB connection failed:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
